8dc705a3fb1714015facae202a250b4e719a74fe
hdhier\hdhier <hsala622@gmail.com>
2022-11-08 19:54:33 +0100

    Añadida logica comparacion de firmas en el handler.

--- Application/Signatures/Commands/CompareSignatures/CompareSignaturesCommand.cs
+++ Application/Signatures/Commands/CompareSignatures/CompareSignaturesCommand.cs
@@ -22,7 +22,45 @@ namespace Application.Signatures.Commands.CompareSignatures
 
         public async Task<string> Handle(CompareSignaturesCommand request, CancellationToken cancellationToken)
         {
-            return "Winner";
+            var contract1Points = 0;
+            var contract2Points = 0;
+
+            contract1Points = GetPoints(request.Contract1);
+            contract2Points = GetPoints(request.Contract2);
+
+            return contract1Points > contract2Points ? "Contract1 wins!" : "Contract2 wins!";
+        }
+
+        private int GetPoints(string contract)
+        {
+            var result = 0;
+
+            var upperContract = contract.ToUpper();
+
+            if (upperContract.Contains("K") && upperContract.Contains("V"))
+            {
+                upperContract = upperContract.Replace("V",string.Empty);
+            }
+
+            foreach (var rol in upperContract)
+            {
+                switch (rol) 
+                {
+                    case 'K':
+                        result += 5;
+                        break;
+                    case 'N':
+                        result += 2;
+                        break;
+                    case 'V':
+                        result += 1;
+                        break;
+                    default:
+                        throw new Exception("Caracter inesperado");
+                }
+            }
+
+            return result;
         }
     }
 }
